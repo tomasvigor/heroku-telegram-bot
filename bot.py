d12282e6aab2e35024bf0ba7d4f828587fc379d0
vigor <tomas.vigor@yandex.ru>
2018-03-13 00:33:18 +0300

    getting updates from bot

--- bot.py
+++ bot.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 #import redis
 import os
-import telebot
+#import telebot
 # import some_api_lib
 # import ...
 
 # Example of your code beginning
 #           Config vars
 token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
+
+
+def get_updates_json():  
+    response = requests.get("https://api.telegram.org/bot{0}/getUpdates".format(token))
+    return response.json()
+	
+print(get_updates_json())
+
+
+
+#some_api_token = os.environ['SOME_API_TOKEN']
 #             ...
 
 # If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
