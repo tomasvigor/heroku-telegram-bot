73f80f267959649db4f4b30d3d78bb0ee94f508f
vigor <tomas.vigor@yandex.ru>
2018-03-13 01:22:23 +0300

    simple echo bot in progress

--- bot.py
+++ bot.py
@@ -1,30 +1,51 @@
 # -*- coding: utf-8 -*-
 
 import os
-import requests
-import time
-
-token = os.environ['TELEGRAM_TOKEN']
-
-
-def get_updates_json():  
-    response = requests.get("https://api.telegram.org/bot{0}/getUpdates".format(token))
-    return response.json()
-print(get_updates_json())
-
-while True:
-    print(get_updates_json())
-    time.sleep(3)
+import logging
+import telegram
+from telegram.error import NetworkError, Unauthorized
+from time import sleep
 
 
+update_id = None
 
-#some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-#r = redis.from_url(os.environ.get("REDIS_URL"))
+def main():
+    """Run the bot."""
+    global update_id
+    # Telegram Bot Authorization Token
+    bot = telegram.Bot(os.environ['TELEGRAM_TOKEN'])
 
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+    # get the first pending update_id, this is so we can skip over it in case
+    # we get an "Unauthorized" exception.
+    try:
+        update_id = bot.get_updates()[0].update_id
+    except IndexError:
+        update_id = None
+
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    while True:
+        try:
+            echo(bot)
+        except NetworkError:
+            sleep(1)
+        except Unauthorized:
+            # The user has removed or blocked the bot.
+            update_id += 1
+
+
+def echo(bot):
+    """Echo the message the user sent."""
+    global update_id
+    # Request updates after the last update_id
+    for update in bot.get_updates(offset=update_id, timeout=10):
+        update_id = update.update_id + 1
+
+        if update.message:  # your bot can receive updates without messages
+            # Reply to the message
+            update.message.reply_text(update.message.text)
+
+
+if __name__ == '__main__':
+    main()
